e248fa43cac173185b33951a2ab2a85e60ccbbdd
Saif Sheikh <ssheikh@shory.com>
2025-12-05 19:43:40 +0400

    feat: ignite app

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement("div",{id: "parent"}, 
     React.createElement("div", {id:"child"}, React.createElement("h1", {}, "This is an H1 tag"))
 );
@@ -9,4 +12,4 @@ const multiParent = React.createElement("div",{id: "parent"},
 const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(multiParent);
+root.render(heading);
